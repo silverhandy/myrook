607dc5f68cc5d19d3ebeb0537ac2680d4a88b97a
Chen, Tingjie <tingjie.chen@intel.com>
2020-06-30 15:44:58 +0800

    Add hammerdb and cosbench benchmark
    Signed-off-by: Chen, Tingjie <tingjie.chen@intel.com>

--- benchmark_on_rook/install.sh
+++ benchmark_on_rook/install.sh
@@ -4,18 +4,18 @@ if [[ "$1" == "local-cockroach" ]]; then
     kubectl apply -f ./cockroachdb/cockroachdb-statefulset-local.yaml
     kubectl apply -f ./cockroachdb/cluster-init.yaml
 
-elif [[ "$1" == "local-rook-csi-cockroach" ]]; then
+elif [[ "$1" == "local-rook-csi" ]]; then
     kubectl apply -f ./rook/common.yaml
     kubectl apply -f ./rook/operator.yaml
     kubectl apply -f local-pv.yaml
     kubectl apply -f local-sc.yaml
     kubectl apply -f ./rook/cluster-on-pvc-local.yaml
     kubectl apply -f ./rook/toolbox.yaml
-    #kubectl apply -f ./rook/cephcsi-storageclass.yaml
+    kubectl apply -f ./rook/cephcsi-storageclass.yaml
     #kubectl apply -f ./cockroachdb/cockroachdb-statefulset-cephcsi.yaml
     #kubectl apply -f ./cockroachdb/cluster-init.yaml
 
-elif [[ "$1" == "meta-rook-csi-cockroach" ]]; then
+elif [[ "$1" == "block-db-rook-csi" ]]; then
     kubectl apply -f ./rook/common.yaml
     kubectl apply -f ./rook/operator.yaml
     kubectl apply -f local-pv.yaml
@@ -26,7 +26,7 @@ elif [[ "$1" == "meta-rook-csi-cockroach" ]]; then
     kubectl apply -f ./cockroachdb/cockroachdb-statefulset-cephcsi.yaml
     kubectl apply -f ./cockroachdb/cluster-init.yaml
 
-elif [[ "$1" == "wal-rook-csi-cockroach" ]]; then
+elif [[ "$1" == "block-wal-rook-csi" ]]; then
     kubectl apply -f ./rook/common-latest.yaml
     kubectl apply -f ./rook/operator-wal.yaml
     kubectl apply -f local-pv.yaml
@@ -37,24 +37,47 @@ elif [[ "$1" == "wal-rook-csi-cockroach" ]]; then
     #kubectl apply -f ./cockroachdb/cockroachdb-statefulset-cephcsi.yaml
     #kubectl apply -f ./cockroachdb/cluster-init.yaml
 
-elif [[ "$1" == "roc-rook-csi-cockroach" ]]; then
+elif [[ "$1" == "block-db-wal-rook-csi" ]]; then
+    kubectl apply -f ./rook/common-latest.yaml
+    kubectl apply -f ./rook/operator-wal.yaml
+    kubectl apply -f local-pv.yaml
+    kubectl apply -f local-sc.yaml
+    kubectl apply -f ./rook/cluster-on-pvc-wal.yaml
+    kubectl apply -f ./rook/toolbox.yaml
+    #kubectl apply -f ./rook/cephcsi-storageclass.yaml
+    #kubectl apply -f ./cockroachdb/cockroachdb-statefulset-cephcsi.yaml
+    #kubectl apply -f ./cockroachdb/cluster-init.yaml
+
+elif [[ "$1" == "roc-rook-csi" ]]; then
     kubectl apply -f ./rook/common.yaml
     kubectl apply -f ./rook/operator.yaml
     kubectl apply -f local-pv.yaml
     kubectl apply -f local-sc.yaml
     kubectl apply -f ./rook/cluster-on-pvc-roc.yaml
     kubectl apply -f ./rook/toolbox.yaml
-    kubectl apply -f ./rook/cephcsi-storageclass.yaml
+    #kubectl apply -f ./rook/cephcsi-storageclass.yaml
+    #kubectl apply -f ./cockroachdb/cockroachdb-statefulset-cephcsi.yaml
+    #kubectl apply -f ./cockroachdb/cluster-init.yaml
+
+else
+    echo "Please make the right choice ~"
+fi
+
+
+if [[ "$1" == "cockroachdb-local" ]]; then
+    kubectl apply -f ./cockroachdb/cockroachdb-statefulset-local.yaml
+    kubectl apply -f ./cockroachdb/cluster-init.yaml
+
+elif [[ "$1" == "cockroachdb-rook-csi" ]]; then
     kubectl apply -f ./cockroachdb/cockroachdb-statefulset-cephcsi.yaml
     kubectl apply -f ./cockroachdb/cluster-init.yaml
 
-elif [[ "$1" == "shutdown" ]]; then
-    #kubectl delete -f local-pv.yaml --grace-period=0 --force
-    #kubectl delete -f local-sc.yaml --grace-period=0 --force
-    kubectl delete -f ./rook/toolbox.yaml --grace-period=0 --force
-    kubectl delete -f ./rook/cluster-on-pvc-local.yaml --grace-period=0 --force
-    kubectl delete -f ./rook/operator.yaml --grace-period=0 --force
-    kubectl delete -f ./rook/common.yaml --grace-period=0 --force
+elif [[ "$1" == "hammerdb-local" ]]; then
+    kubectl apply -f ./hammerdb/hammerdb-statefulset-local.yaml
+
+elif [[ "$1" == "hammerdb-local" ]]; then
+    kubectl apply -f ./hammerdb/hammerdb-statefulset-cephcsi.yaml
+
 else
-    echo "Please make the right choice ~"
+    echo "Please make the right choice ..."
 fi
